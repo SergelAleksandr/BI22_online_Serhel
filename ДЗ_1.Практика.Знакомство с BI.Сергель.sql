38ec8494b7293d1a9c8b2487d467f9f707e88c12
SergelAleksandr <64100564+SergelAleksandr@users.noreply.github.com>
2024-06-02 17:52:22 +0300

    Add files via upload

---
+++ ДЗ_1.Практика.Знакомство с BI.Сергель.sql
@@ -0,0 +1,17 @@
+-- a)
+--SELECT * FROM Sales.SalesTerritory
+
+-- b)
+--SELECT TerritoryID, Name
+--FROM Sales.SalesTerritory
+
+--c)
+--SELECT * FROM Person.Person
+--WHERE LastName = 'Norman'
+
+--d)
+--SELECT * FROM Person.Person
+--WHERE EmailPromotion != 2
+
+--SELECT * FROM Person.Person
+--WHERE EmailPromotion <>2
